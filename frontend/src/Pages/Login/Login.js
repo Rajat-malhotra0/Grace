84b043182a8e43130f630a6b06dcd6a1d726c244
Satyarth-Sahu17 <sahusatyartha17@gmail.com>
2025-07-19 09:14:33 +0530

    Added LoginValidation

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -5,6 +5,7 @@ import "./Login.css";
 
 function Login() {
     const [formData, setFormData] = useState({});
+    const [errors, setErrors] = useState({});
 
     const handleChange = (e) => {
         setFormData((prevData) => ({
@@ -12,10 +13,31 @@ function Login() {
             [e.target.name]: e.target.value,
         }));
     };
-
+    const validate = () => {
+        const errors = {};
+        const {
+            email = '',
+            password = ''
+        } = formData;
+        if (!email.trim()) {
+            errors.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            errors.email = "Email is invalid";
+        }
+        if (!password.trim()) {
+            errors.password = "Password is required";
+        } else if (password.length < 8) {
+            errors.password = "Password must be 8 characters long"
+        }
+        return errors;
+    };
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        const validationErrors = validate();
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+            return;
+        }
         const payload = {
             email: formData.email,
             password: formData.password,
@@ -43,25 +65,29 @@ function Login() {
                     <h1>Welcome Back!</h1>
                     <p>It's good to see you again.</p>
                 </div>
-
                 <form className="login-form" onSubmit={handleSubmit}>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email Address"
-                        onChange={handleChange}
-                        value={formData.email || ""}
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        onChange={handleChange}
-                        value={formData.password || ""}
-                    />
+                    <div className="input-group">
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email Address"
+                            onChange={handleChange}
+                            value={formData.email || ""}
+                        />
+                        <div style={{ color: 'red' }}>{errors.email}</div>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            value={formData.password || ""}
+                        />
+                        <div style={{ color: 'red' }}>{errors.password}</div>
+                    </div>
                     <button type="submit">Log In</button>
                 </form>
-
                 <div className="register-link">
                     <span>
                         Need an account? <Link to="/register">Register</Link>
